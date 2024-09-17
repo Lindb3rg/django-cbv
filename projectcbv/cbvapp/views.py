50d44d882343b38d329eced09be9b3a7c428ced7
lindberg <ad.lindberg@gmail.com>
2024-09-17 21:14:36 +0200

    added some html

--- projectcbv/cbvapp/views.py
+++ projectcbv/cbvapp/views.py
@@ -1,21 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView
+from django.views.generic import View,TemplateView,ListView,DetailView
+
+from . import models
 
 
 
+class SchoolListView(ListView):
+    model = models.School
 
 
-
-
-class IndexView(TemplateView):
-    template_name = 'index.html'
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['injected_content'] = "CONTENT!"
-        return context
-
-
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'cbvapp/school_detail.html'
 
 
         
